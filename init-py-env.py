f16e88bc16f97ff8b6ccbb2a35410173808ad333
wocin <925511765@qq.com>
2014-01-21 13:32:50 +0800

    init py env

--- init-py-env.py
+++ init-py-env.py
@@ -4,24 +4,24 @@
 #init python env
 #-------------------I am boring line------------------------------------
 import os
-import subprocess
 
 def init_first():#init some soft for next
-	os.system("yum -y install git python-setuptools")
-	os.system("git clone")
-def init_pip():#init pip
-	os.system("cd pip")
-	os.system("python setup.py install")
-def init_virt_fab():#init virtualenv and fabric
-
-def init_salt_ansbile():#init saltstrack and ansible
-
-def clean():#Delete the installation package
-
+	code=os.system("which pip")
+	if code == 0:
+		print "pip tools has installed on this system"
+	else:
+		os.system("yum -y install git python-setuptools python-devel python-docs autoconf gcc")
+		os.system("git clone https://github.com/pypa/pip.git")
+		os.system("python pip/setup.py build")
+		os.system("python pip/setup.py install")
+		os.system("mkdir /root/.pip/")
+		os.system("touch /root/.pip/pip.conf")
+		fpip=open("/root/.pip/pip.conf",'w')
+		fpip.write("[global]\nindex-url=http://pypi.v2ex.com/simple\n")
+		fpip.close()
+		
 def main():
 	init_first()
-	init_pip()
-	init_virt_fab()
-	init_salt_ansible()
+	
 if __name__ == '__main__':
 	main()
