df4e31cebcafbe107889df9ddeceee2f1a0c61e1
wocin <925511765@qq.com>
2014-01-21 13:43:19 +0800

    init py module

--- init-py-module.py
+++ init-py-module.py
@@ -3,3 +3,15 @@
 #Email ---
 #choice init python module
 #-------------------I am boring line------------------------------------
+import sys,subprocess
+
+def init(mod):
+	code=subprocess.call('python -c "import %s"' %mod,shell=True)
+	if code == 0:
+		print '%s module has installed' %mod
+	else:
+		subprocess.call('pip install %s' %mod,shell=True)
+
+if __name__ == '__main__':
+	mod=sys.argv[1]
+	init(mod)
