fb6032eca6603c349573f0e431426c9cd7da4285
wocin <925511765@qq.com>
2014-01-21 19:53:09 +0800

    creat choice-trust-host.py file

--- choice-trust-host.py
+++ choice-trust-host.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
-#By wocin
-#Email ---
-#init ssh-key and trust remote host
-#-------------------I am boring line------------------------------------
+#===============================================================================
+#
+#          FILE: choice-trust-host.py
+# 
+#         USAGE: python choice-trust-host.py
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: wocin, 
+#  ORGANIZATION: 
+#       CREATED: 
+#      REVISION:  ---
+#===============================================================================
